ce480810f75f3a5579432f37a2ea580635481ef7
loxt <emannuelloxt@hotmail.com>
2021-01-30 13:09:59 -0400

    feat(setup): nestjs and graphql library

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from '../environments/environment';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './database.config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { LIB_PATH } from '@nx-space-explorer/graphql';
+
+const lib = join(process.cwd(), LIB_PATH);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [environment],
+    }),
+    GraphQLModule.forRoot({
+      typePaths: [join(lib, 'schemas/**/*.graphql')],
+      definitions: {
+        path: join(lib, 'lib/graphql.ts'),
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/server/src/environments/environment.ts
+++ apps/server/src/environments/environment.ts
@@ -1,3 +1,19 @@
-export const environment = {
+import { EntitySchema } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const environment = () => ({
   production: false,
-};
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  database: (entities: EntitySchema[]): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities,
+  }),
+});
